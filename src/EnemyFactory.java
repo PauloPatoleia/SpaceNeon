49e3bb52cb480f23e69e4faaf6fb6ef21eed2ebf
David Vaz <davidzav13@gmail.com>
2019-02-09 20:04:02 +0000

    Make enum local class

--- src/EnemyFactory.java
+++ src/EnemyFactory.java
@@ -7,30 +7,30 @@ public class EnemyFactory {
 
         if (randomEnemy == 0) {
 
-            int randomEnemyNumber = (int) (Math.random() * (EnemyType.values()).length);
+            int randomEnemyNumber = (int) (Math.random() * (EnemyShooter.EnemyType.values()).length);
 
-            switch (EnemyType.values()[randomEnemyNumber]) {
+            switch (EnemyShooter.EnemyType.values()[randomEnemyNumber]) {
                 case ENEMY_ONE:
                     int randomOne = (int) (Math.random() * 751 + 0);
-                    return new EnemyShooter(randomOne, 0, EnemyType.ENEMY_ONE);
+                    return new EnemyShooter(randomOne, 0, EnemyShooter.EnemyType.ENEMY_ONE);
                 case ENEMY_TWO:
                     int randomTwo = (int) (Math.random() * 731 + 0);
-                    return new EnemyShooter(randomTwo, 0, EnemyType.ENEMY_TWO);
+                    return new EnemyShooter(randomTwo, 0, EnemyShooter.EnemyType.ENEMY_TWO);
 
             }
         }
 
         if (randomEnemy == 1) {
 
-            int randomObstacleNumber = (int) (Math.random() * (ObstacleType.values().length));
+            int randomObstacleNumber = (int) (Math.random() * (Obstacle.ObstacleType.values().length));
 
-            switch (ObstacleType.values()[randomObstacleNumber]) {
+            switch (Obstacle.ObstacleType.values()[randomObstacleNumber]) {
                 case OBSTACLE_ONE:
                     int randomOne = (int) (Math.random() * 701 + 0);
-                    return new Obstacle(randomOne, 0, ObstacleType.OBSTACLE_ONE);
+                    return new Obstacle(randomOne, 0, Obstacle.ObstacleType.OBSTACLE_ONE);
                 case OBSTACLE_TWO:
                     int randomTwo = (int) (Math.random() * 681 + 0);
-                    return new Obstacle(randomTwo, 0, ObstacleType.OBSTACLE_TWO);
+                    return new Obstacle(randomTwo, 0, Obstacle.ObstacleType.OBSTACLE_TWO);
 
             }
         }
--- src/Obstacle.java
+++ src/Obstacle.java
@@ -21,4 +21,32 @@ public class Obstacle extends Enemy {
     public void render() {
         obstacle.draw();
     }
+
+    public enum ObstacleType {
+
+        OBSTACLE_ONE(1,2, "./Resources/obstacle100.jpg"),
+        OBSTACLE_TWO(2,2, "./Resources/obstacle120.jpg");
+
+        private int velocity;
+        private String image;
+        private int hp;
+
+        ObstacleType(int velocity, int hp, String image){
+            this.velocity = velocity;
+            this.hp = hp;
+            this.image = image;
+        }
+
+        public int getVelocityY(){
+            return velocity;
+        }
+
+        public String getImage() {
+            return image;
+        }
+
+        public int getHp() {
+            return hp;
+        }
+    }
 }
