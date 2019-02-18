4161c4883445552db01a5f1000c0704a1a2302b7
PauloPatoleia <vmpatoleia@protonmail.com>
2019-02-18 17:59:54 +0000

    fix music path

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Sound sound = new Sound("/resources/music.wav");
+        Sound sound = new Sound("music.wav");
         sound.play(true);
         sound.setLoop(5);
 
